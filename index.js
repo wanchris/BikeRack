bdc26fc0cb82f6752dc4837a88c5efbe6106a2b3
bryantung97 <bryantung97@hotmail.com>
2017-12-03 11:05:03 -0500

    testing changes

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var http= require('http');
+var mongoose = require('mongoose');
 
-app.set('port', (process.env.PORT || 5000));
+var uristring = prcoess.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/index';
+
+var theport = process.env.PORT || 5000;
+
+
+mongoose.connect(uristring, function (err, res){
+  if (err){
+    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+  }
+  else{
+    console.log('Succeeded connected to: ' + uristring);
+  } 
+});
+
+app.set('port', theport);
 
 app.use(express.static(__dirname + '/public'));
 
