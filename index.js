7f6bb83e94b8bb2ec3da72ef084201c03ee72ca6
Chris Wan <ccc.wan@mail.utoronto.ca>
2017-12-04 13:51:28 -0500

    Restotring index.js

--- index.js
+++ index.js
@@ -1,87 +1,134 @@
-$(function(){
-    $(".view").hide();
-    $("#view-main").show();
-    $(this).scrollTop(0);
-    function display(view){
-        $(".view").hide();
-        $("#" + view).show();
-    }
-    $(".view-btn").click(function(){
-        display("view-" + this.id);
-    });
+//setup =================================================
+var express = require('express');
+var path = require('path');
+var http= require('http');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+var passport = require('passport');
+var pg = require('pg');
+var flash = require('connect-flash');
+//var User = require('./models/users');
+var uristring = process.env.MONGODB_URI || process.env.MONGOLAB_RED_URI || 'mongodb://localhost/';
+var theport = process.env.PORT || 5000;
+var uristring = 'mongodb://heroku_f9nb6r1s:a0ojrjrdrr6at3br8s6efvuo35@ds129796.mlab.com:29796/heroku_f9nb6r1s';
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
 
-    $("#submitSearch").click(enterPress);
+const mainController = require('./controllers/main.controller.js');
+
+mongoose.connect(uristring, function (err, res){
+  if (err){
+    console.log('ERROR connecting to: ' + uristring + '. ' + err);
+  }
+  else{
+    console.log('Succeeded connected to: ' + uristring);
+  } 
 });
 
-var enterPress = function(){
-    var e = $.Event("keypress");
-    e.which = 13;
-    $("#searchBox").trigger(e);
-}
+
+//config ==============================================
+require('./passport')(passport); 
+app.use(expressValidator());
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('.html', require('ejs').__express);
+
+app.use(session({
+  secret: 'apfepaijfpoaijsopefi',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.set('port', theport);
+
+app.use(express.static(__dirname + '/public'));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
 
 
-//Switch dropdown text
-function dropdown(val) {
-    var y = document.getElementsByClassName('btn btn-default dropdown-toggle');
-    var aNode = y[0].innerHTML = val + ' <span class="caret"></span>';
-}
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+app.get('/', function(request, response) {
+  response.render('pages/index')
+})
+    app.get('/index', function(req, res) {
+        res.render('pages/index');  
+    })
 
-function convertToString(val){
-    return JSON.stringify(val, null, "\t");
-}
+    app.get('/login', function(req, res) {
+        res.render('pages/login');
+    })
 
-window.onload=function(){
-    //Pressed Enter key on Search bar      
-    var dropVal = "City";
-    var input = document.getElementById("searchBox").onkeypress = function(e){
-        var event =  e || window.event;
-        var code = event.which || event.keyCode;
-        if(code == '13'){   
-            dropVal = document.getElementById("searchBtn").textContent; 
-            var ourRequest = new XMLHttpRequest();
-            ourRequest.open('GET',"http://api.citybik.es/v2/networks/");
-            ourRequest.onload = function(){
-                var ourData = JSON.parse(ourRequest.responseText);
-                renderHTML(ourData);
-            };
-            ourRequest.send();
-        }
+    app.get('/signup', function(req, res) {
+        res.render('pages/signup', {
+            message: req.flash('signupMessage'),
+            loggedIn: req.user,
+            user: req.user,
+            status: 'signup',
+
+        });
+    })
+
+    app.get('/search', mainController.getSearch);
+
+    app.post('/locations/newId', mainController.newLocation);
+
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/user', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/user', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+        return true;
+    } else {
+        res.redirect('/login');
+        return false;
     }
-    //Show search result output
-    var output = document.getElementById("output");
-    function renderHTML(data){
-        var returnString = ""
-        var curJSON = {};
-        var curString ="";
-        for(i=0; i<data.networks.length; i++){
-            curJSON = data.networks[i];
-            if(dropVal.trim() == "City"){
-                curString = JSON.stringify(curJSON.location.city, null, "\t");
-            }
-            else if(dropVal.trim() == "Country"){
-                curString = JSON.stringify(curJSON.location.country, null, "\t");
-            }
-            else if(dropVal.trim() == "Company"){
-                if (curJSON.company == null){
-                    continue;
-                }
-                for (j=0; j<curJSON.company.length; j++){
-                    curString += curJSON.company[j];
-                }
-            }
-            else{
-                if (curString.indexOf(document.getElementById("searchBox").value) != -1){
-                    returnString += "<p>" + JSON.stringify(data.networks[i].location, null, "\t") + "</p>";
-                }
-                returnString += "<p>" + JSON.stringify(data.networks[i].location, null, "\t") + "</p>";
-            }
-            if (curString.indexOf(document.getElementById("searchBox").value) != -1){
-                returnString += "<p>" + "Name: " + convertToString(curJSON.name) + 
-                    " City: " + convertToString(curJSON.location.city) + " Country: " 
-                    + convertToString(curJSON.location.country) + "</p>";
-            }
-        }
-        //returnString = JSON.stringify(data.networks.company);
-        output.insertAdjacentHTML('beforeend', returnString);
-    }
 }
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        req.session.save(function() {
+            res.redirect('/login');
+        });
+    }); 
+
+app.get('/user', function(req, res) {
+        const errors = req.validationErrors();
+        if (errors){
+            req.flash('errors', errors.map(err =>err.msg));
+            res.redirec('/user');
+        }
+        //console.log(req.user.local.username);
+        res.render('pages/user', {
+            user: req.user.local.username
+        });
+    })
+
+   // app.post('/search/newUser', mainController.createUser);
